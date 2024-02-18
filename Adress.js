6270a18da09fabb3b510ae4565984490bcd5cb9e
Rai <raisilm@hotmail.com>
2024-02-17 22:32:08 -0300

    fixing visual v1.2

--- Adress.js
+++ Adress.js
@@ -1,35 +1,38 @@
-export let adress = ["Miritiba 339", "Babaçu 500", "Cambui 804B", "Rio Branco 23", "Quirino dos Santos 23 b", "4, Rue de la République", "Calle Sagasta, 26", "Calle 44 No 1991", "100 Broadway Av" ];
- let adressSplit = [];
+export let adress = ["Miritiba 339", "Babaçu 500", "Cambui 804B", "Rio Branco 23", "Quirino dos Santos 23 b", "4, Rue de la République", "Calle Sagasta, 26", "Calle 44 No 1991", "100 Broadway Av"];
+let adressSplit = [];
 export let verifyAdress = () => {
-   adress.forEach(adressLocal => {
-       if(adressLocal.split(" ").length < 3)
-        adressSplit.push(adressLocal.split(" "));
-        else if(adressLocal.split(", ").length == 2){
+
+    adressSplit = [];
+
+    adress.forEach(adressLocal => {
+        if (adressLocal.split(" ").length < 3)
+            adressSplit.push(adressLocal.split(" "));
+        else if (adressLocal.split(", ").length == 2) {
             let currentAdress = adressLocal.split(", ");
-            !(currentAdress[0]%1==0) ? undefined : currentAdress.reverse(); 
+            !(currentAdress[0] % 1 == 0) ? undefined : currentAdress.reverse();
             adressSplit.push(currentAdress);
         }
-        else if(adressLocal.match(/(No)\s\d+/)){
-           let currentAdress = [];
-           currentAdress.push(adressLocal.slice(0, adressLocal.match(/(No)\s\d+/)["index"]-1));
-           currentAdress.push(adressLocal.match(/(No)\s\d+/)[0]);
-           adressSplit.push(currentAdress);
-       }else{
-           let adressInfo = adressLocal.match(/\d+(\s[a-z])?/);
-           let initial, end;
-           if(adressInfo["index"] == 0){ 
-               initial = adressInfo[0].length+1;
-               end = adressLocal.length
-           }else{
-               initial = 0;
-               end = adressInfo["index"] -1;
-           }
-           let currentAdress= [];
-           currentAdress.push(adressLocal.slice(initial, end))
-           currentAdress.push(adressInfo[0]);
-           adressSplit.push(currentAdress)
-       }
-   })
-   return adressSplit
- }
- console.log(verifyAdress());
+        else if (adressLocal.match(/(No)\s\d+/)) {
+            let currentAdress = [];
+            currentAdress.push(adressLocal.slice(0, adressLocal.match(/(No)\s\d+/)["index"] - 1));
+            currentAdress.push(adressLocal.match(/(No)\s\d+/)[0]);
+            adressSplit.push(currentAdress);
+        } else {
+            let adressInfo = adressLocal.match(/\d+(\s[a-z])?/);
+            let initial, end;
+            if (adressInfo["index"] == 0) {
+                initial = adressInfo[0].length + 1;
+                end = adressLocal.length
+            } else {
+                initial = 0;
+                end = adressInfo["index"] - 1;
+            }
+            let currentAdress = [];
+            currentAdress.push(adressLocal.slice(initial, end))
+            currentAdress.push(adressInfo[0]);
+            adressSplit.push(currentAdress)
+        }
+    })
+    return adressSplit
+}
+console.log(verifyAdress());
--- main.js
+++ main.js
@@ -2,19 +2,21 @@ import { verifyAdress } from "./Adress.js";
 
 let corpoPag = document.body;
 let tabela = document.createElement("table");
+tabela.setAttribute('border', '1px' );
+tabela.style.margin = "0 auto"
 
 tabela.innerHTML = `
     <tr> 
-        <th>Rua</th>
-        <th>Número</th>
+        <th style="padding: 10px">Rua</th>
+        <th style="padding: 10px">Número</th>
     </tr>
 `
-
+console.log(verifyAdress())
 verifyAdress().forEach(Adress => {
     tabela.innerHTML += `
         <tr> 
-            <td>${Adress[0]}</td>
-            <td>${Adress[1]}</td>
+            <td style="padding: 10px">${Adress[0]}</td>
+            <td style="padding: 10px">${Adress[1]}</td>
         </tr>
     `
 });
